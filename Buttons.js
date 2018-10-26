df1a9d612db732b80ece286060fd99979e445aca
nataliakocol <natalia.kocol@ada.ac.uk>
2018-10-26 10:45:27 +0100

    some changes

---
+++ Buttons.js
@@ -0,0 +1,10 @@
+class Button {
+  constructor(label, x, y) {
+    //console.log('making a new button ', label)
+    this.label = label;
+    this.x = x;
+    this.y = y;
+    console.log(this);
+  }
+  //console.log(this);
+}
